d9f8b564f424956fb43090a59e02d25d12a51d48
Qinyi Ding <thomasdqy@hotmail.com>
2014-08-29 22:56:18 -0400

    test1

---
+++ app/src/main/java/qding/com/smartglass/LocationLiveCardService.java
@@ -0,0 +1,52 @@
+package qding.com.smartglass;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.LiveCard.PublishMode;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * A {@link Service} that publishes a {@link LiveCard} in the timeline.
+ */
+public class LocationLiveCardService extends Service {
+
+    private static final String LIVE_CARD_TAG = "LocationLiveCardService";
+
+    private LiveCard mLiveCard;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mLiveCard == null) {
+            mLiveCard = new LiveCard(this, LIVE_CARD_TAG);
+
+            LiveCardRenderer renderer = new LiveCardRenderer(this);
+            mLiveCard.setDirectRenderingEnabled(true).getSurfaceHolder().addCallback(renderer);
+
+            // Display the options menu when the live card is tapped.
+            Intent menuIntent = new Intent(this, LiveCardMenuActivity.class);
+            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+            mLiveCard.attach(this);
+            mLiveCard.publish(PublishMode.REVEAL);
+        } else {
+            mLiveCard.navigate();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mLiveCard != null && mLiveCard.isPublished()) {
+            mLiveCard.unpublish();
+            mLiveCard = null;
+        }
+        super.onDestroy();
+    }
+}
